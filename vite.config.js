58e45dfaf4808a639d0a75eed24488295da48cdb
YUGESHKARAN <yugeshkaran01@gmail.com>
2024-11-04 12:00:24 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Use './' if deploying to a subdirectory or custom base path
   build: {
     outDir: 'dist' // Ensure this matches the "distDir" in vercel.json
   }
